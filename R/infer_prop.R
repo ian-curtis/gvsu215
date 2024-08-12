4946433486b4a8b5382551c5faf1fefcf9c85d34
Andrew DiLernia <andrew.s.dilernia@gmail.com>
2024-08-12 14:57:31 -0400

    Update infer_prop.R to fix column widths issue

--- R/infer_prop.R
+++ R/infer_prop.R
@@ -15,63 +15,63 @@
 #' infer_1prop_int(mtcars, ~vs, success = 1)
 #' infer_1prop_int(mtcars, ~vs, success = 1, conf_lvl = 0.90)
 infer_1prop_int <- function(data, formula, success = NULL, digits = 3, conf_lvl = 0.95, caption = NULL) {
-
+  
   # check for empty strings and make them actual NAs
   data <- tibble::as_tibble(data) %>%
     dplyr::mutate(dplyr::across(dplyr::where(is.character), ~dplyr::na_if(., "")))
-
+  
   # error catching
   check_conf_lvl(conf_lvl)
-
+  
   if (base::is.null(success)) {
-
+    
     cli::cli_abort("A value for what a success is in this situation must be provided.")
-
+    
   }
-
+  
   check_test(mosaic::prop.test(formula, data = data, conf.level = conf_lvl, success = success))
-
+  
   #code
-
+  
   var <- formula[[2]]
   var_str <- base::deparse(base::substitute(var))
-
+  
   # build caption
   if (base::is.null(caption)) {
-
+    
     caption <- base::paste("One-Sample Proportion Confidence Interval on Variable", var_str,
                            "\n Successes:", success)
-
+    
   } else {
-
+    
     caption <- base::paste(caption, "\n Successes:", success)
-
+    
   }
-
+  
   prop_test <- mosaic::prop.test(formula, data = data, conf.level = conf_lvl, success = success, correct = FALSE)
-
+  
   cl <-  base::paste0(conf_lvl*100, "%")
-
+  
   n_success <- data %>%
     dplyr::filter({{ var }} == success) %>%
     base::nrow()
-
+  
   n_na <- find_na(data, formula)
-
+  
   phat <- prop_test$estimate[[1]]
   n <- base::nrow(data) - n_na
   se <- base::sqrt((phat * (1 - phat)) / n)
   z_star <- round(stats::qnorm((1 - conf_lvl) / 2, lower.tail = FALSE), 3)
   moe <- se*z_star
-
-
+  
+  
   broom::tidy(prop_test) %>%
     dplyr::mutate(n_success = n_success,
-           na = n_na,
-           n = n,
-           se = se,
-           conf.low = estimate - moe,
-           conf.high = estimate + moe
+                  na = n_na,
+                  n = n,
+                  se = se,
+                  conf.low = estimate - moe,
+                  conf.high = estimate + moe
     ) %>%
     dplyr::select(n_success, na, n, estimate, se, conf.low, conf.high) %>%
     finalize_tbl(digits = digits,
@@ -80,8 +80,10 @@ infer_1prop_int <- function(data, formula, success = NULL, digits = 3, conf_lvl 
     flextable::set_header_labels(n_success = "n Successes", na = "n Missing",
                                  estimate = "p\u0302", se = "Standard Error",
                                  conf.low = base::paste(cl, "Interval Lower"),
-                                 conf.high = base::paste(cl, "Interval Upper"))
-
+                                 conf.high = base::paste(cl, "Interval Upper")) %>% 
+    flextable::autofit() %>% 
+    flextable::fit_to_width(max_width = 7.5)
+  
 }
 
 #' Create a summary table for a one-sample proportion test
@@ -98,61 +100,61 @@ infer_1prop_int <- function(data, formula, success = NULL, digits = 3, conf_lvl 
 #' infer_1prop_test(mtcars, ~vs, success = 1, conf_lvl = 0.90)
 #' infer_1prop_test(mtcars, ~vs, success = 1, p0 = 0.4)
 infer_1prop_test <- function(data, formula, success = NULL, p0 = 0.5, digits = 3, conf_lvl = 0.95, caption = NULL) {
-
+  
   # check for empty strings and make them actual NAs
   data <- tibble::as_tibble(data) %>%
     dplyr::mutate(dplyr::across(dplyr::where(is.character), ~dplyr::na_if(., "")))
-
+  
   # error catching
   check_conf_lvl(conf_lvl)
-
+  
   if (base::is.null(success)) {
-
+    
     cli::cli_abort("A value for what a success is in this situation must be provided.")
-
+    
   }
-
+  
   check_test(mosaic::prop.test(formula, data = data, p = p0, conf.level = conf_lvl, success = success))
-
+  
   #code
-
+  
   var <- formula[[2]]
   var_str <- base::deparse(base::substitute(var))
-
+  
   # build caption
   if (base::is.null(caption)) {
-
+    
     caption <- base::paste("One-Sample Proportion Test on Variable", var_str,
                            "\n Successes:", success,
                            "\n Null Value:", p0)
-
+    
   } else {
-
+    
     caption <- base::paste(caption, "\n Success:", success,
                            "\n Null Value:", p0)
-
+    
   }
-
+  
   prop_test <- mosaic::prop.test(formula, data = data, p = p0, conf.level = conf_lvl, success = success)
-
+  
   cl <-  base::paste0(conf_lvl*100, "%")
-
+  
   n_success <- data %>%
     dplyr::filter({{ var }} == success) %>%
     base::nrow()
-
+  
   n_na <- find_na(data, formula)
-
+  
   n <- base::nrow(data) - n_na
-
+  
   ### Manual Calculations for Wald's Method not Wilson's ###
-
+  
   z_num <- prop_test$estimate - p0
   se <- base::sqrt((p0*(1 - p0)) / n)
   z <- z_num / se
-
+  
   pval <- stats::pnorm(q = z, lower.tail = base::ifelse(z <= 0, TRUE, FALSE)) * 2
-
+  
   broom::tidy(prop_test) %>%
     dplyr::mutate(n_success = n_success,
                   na = n_na,
@@ -168,8 +170,10 @@ infer_1prop_test <- function(data, formula, success = NULL, p0 = 0.5, digits = 3
                  caption = caption,
                  striped = FALSE) %>%
     flextable::set_header_labels(n_success = "n Successes", na = "n Missing", n = "n Used",
-                                 estimate = "p\u0302", se = "Standard Error", pval = "p-value (2 tail)")
-
+                                 estimate = "p\u0302", se = "Standard Error", pval = "p-value (2 tail)") %>% 
+    flextable::autofit() %>% 
+    flextable::fit_to_width(max_width = 7.5)
+  
 }
 
 
@@ -185,47 +189,47 @@ infer_1prop_test <- function(data, formula, success = NULL, p0 = 0.5, digits = 3
 #' infer_2prop_int(mtcars, vs~am, success = 1)
 #' infer_2prop_int(mtcars, vs~am, success = 1, conf_lvl = .9)
 infer_2prop_int <- function(data, formula, success, digits = 3, conf_lvl = 0.95, caption = NULL) {
-
+  
   # check for empty strings and make them actual NAs
   data <- tibble::as_tibble(data) %>%
     dplyr::mutate(dplyr::across(dplyr::where(is.character), ~dplyr::na_if(., "")))
-
+  
   # error catching
   check_conf_lvl(conf_lvl)
-
+  
   if (base::is.null(success)) {
-
+    
     cli::cli_abort("A value for what a success is in this situation must be provided.")
-
+    
   }
-
+  
   var1 <- formula[[2]]
   var1_str <- base::deparse(base::substitute(var1))
-
+  
   grp_var <- formula[[3]]
   grp_str <- base::deparse(base::substitute(grp_var))
-
+  
   check_test(mosaic::prop.test(formula, data = data, conf.level = conf_lvl, success = success, correct = FALSE))
-
+  
   # code
   two_prop <- mosaic::prop.test(formula, data = data, conf.level = conf_lvl, success = success, correct = FALSE)
-
+  
   # build caption
   if (base::is.null(caption)) {
-
+    
     caption <- base::paste("Two Sample Proportion Interval Between", var1_str, "and", grp_str,
                            "\n Successes:", success)
-
+    
   } else {
-
+    
     caption <- base::paste(caption, "\n Successes:", success)
-
+    
   }
-
+  
   cl <- base::paste0(conf_lvl*100, "%")
-
+  
   grp_lvls <- sort(unique(dplyr::pull(data, grp_var)))
-
+  
   # find NAs
   na1 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[1] & base::is.na({{ grp_var }})) %>%
@@ -233,7 +237,7 @@ infer_2prop_int <- function(data, formula, success, digits = 3, conf_lvl = 0.95,
   na2 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[2] & base::is.na({{ grp_var }})) %>%
     base::nrow()
-
+  
   # find sample sizes
   n1 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[1]) %>%
@@ -241,7 +245,7 @@ infer_2prop_int <- function(data, formula, success, digits = 3, conf_lvl = 0.95,
   n2 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[2]) %>%
     base::nrow()
-
+  
   # find success
   yay1 <- data %>%
     dplyr::select({{ var1 }}, {{ grp_var }}) %>%
@@ -251,15 +255,15 @@ infer_2prop_int <- function(data, formula, success, digits = 3, conf_lvl = 0.95,
     dplyr::select({{ var1 }}, {{ grp_var }}) %>%
     dplyr::filter({{ var1 }} == success & {{ grp_var }} == grp_lvls[2]) %>%
     mosaic::tally()
-
+  
   # manual calculations for STA 215
-
+  
   se1 <- (two_prop$estimate[[1]]*(1 - two_prop$estimate[[1]])) / n1
   se2 <- (two_prop$estimate[[2]]*(1 - two_prop$estimate[[2]])) / n2
   se <- sqrt(se1 + se2)
   z_star <- round(stats::qnorm((1 - conf_lvl) / 2, lower.tail = FALSE), 3)
   moe <- se*z_star
-
+  
   # build actual table
   tibble::tibble(
     var = base::as.character(grp_lvls),
@@ -277,8 +281,10 @@ infer_2prop_int <- function(data, formula, success, digits = 3, conf_lvl = 0.95,
     flextable::set_header_labels(var = grp_str, yay = "n Successes", na = "n Missing", phat = "p\u0302",
                                  se = "Standard Error", cil = base::paste(cl, "Interval Lower"),
                                  ciu = base::paste(cl, "Interval Upper")) %>%
-    flextable::vline(j = 5)
-
+    flextable::vline(j = 5) %>% 
+    flextable::autofit() %>% 
+    flextable::fit_to_width(max_width = 7.5)
+  
 }
 
 #' Create a summary table for a two-sample proportion test
@@ -292,42 +298,42 @@ infer_2prop_int <- function(data, formula, success, digits = 3, conf_lvl = 0.95,
 #' infer_2prop_test(mtcars, vs~am, success = 1)
 #' infer_2prop_test(mtcars, vs~am, success = 1, conf_lvl = .9, digits = 4)
 infer_2prop_test <- function(data, formula, success, digits = 3, conf_lvl = 0.95, caption = NULL) {
-
+  
   # check for empty strings and make them actual NAs
   data <- tibble::as_tibble(data) %>%
     dplyr::mutate(dplyr::across(dplyr::where(is.character), ~dplyr::na_if(., "")))
-
+  
   # error catching
   check_conf_lvl(conf_lvl)
-
+  
   check_test(mosaic::prop.test(formula, data = data, conf.level = conf_lvl, success = success, correct = FALSE))
-
-
+  
+  
   # code
   two_prop <- mosaic::prop.test(formula, data = data, conf.level = conf_lvl, success = success, correct = FALSE)
-
+  
   var1 <- formula[[2]]
   var1_str <- base::deparse(base::substitute(var1))
-
+  
   grp_var <- formula[[3]]
   grp_str <- base::deparse(base::substitute(grp_var))
-
+  
   cl <- base::paste0(conf_lvl*100, "%")
-
+  
   # build caption
   if (base::is.null(caption)) {
-
+    
     caption <- base::paste("Two Sample Proportion Test Between", var1_str, "and", grp_str,
                            "\n Successes:", success, "| Confidence:", cl)
-
+    
   } else {
-
+    
     caption <- base::paste(caption, "\n Successes:", success, "| Confidence:", cl)
-
+    
   }
-
+  
   grp_lvls <- sort(unique(dplyr::pull(data, grp_var)))
-
+  
   # find NAs
   na1 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[1] & base::is.na({{ grp_var }})) %>%
@@ -335,7 +341,7 @@ infer_2prop_test <- function(data, formula, success, digits = 3, conf_lvl = 0.95
   na2 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[2] & base::is.na({{ grp_var }})) %>%
     base::nrow()
-
+  
   # find sample sizes
   n1 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[1]) %>%
@@ -343,7 +349,7 @@ infer_2prop_test <- function(data, formula, success, digits = 3, conf_lvl = 0.95
   n2 <- data %>%
     dplyr::filter({{ grp_var }} == grp_lvls[2]) %>%
     base::nrow()
-
+  
   # find successes
   yay1 <- data %>%
     dplyr::select({{ var1 }}, {{ grp_var }}) %>%
@@ -353,11 +359,11 @@ infer_2prop_test <- function(data, formula, success, digits = 3, conf_lvl = 0.95
     dplyr::select({{ var1 }}, {{ grp_var }}) %>%
     dplyr::filter({{ var1 }} == success & {{ grp_var }} == grp_lvls[2]) %>%
     mosaic::tally()
-
+  
   se1 <- (two_prop$estimate[[1]]*(1 - two_prop$estimate[[1]])) / n1
   se2 <- (two_prop$estimate[[2]]*(1 - two_prop$estimate[[2]])) / n2
   se <- sqrt(se1 + se2)
-
+  
   # build table
   # tk check z, cil, and ciu (and point estimate)
   tibble::tibble(
@@ -369,16 +375,16 @@ infer_2prop_test <- function(data, formula, success, digits = 3, conf_lvl = 0.95
     se = c(se, NA),
     z = c(two_prop$statistic, NA),
     p = c(base::ifelse(two_prop$p.value < 0.0001,
-                 "< 0.0001",
-                 base::format.pval(two_prop$p.value, digits = digits)), NA)
+                       "< 0.0001",
+                       base::format.pval(two_prop$p.value, digits = digits)), NA)
   ) %>%
     finalize_tbl(digits = digits,
                  caption = caption,
                  na_str = "") %>%
     flextable::set_header_labels(var = "Variable", yay = "n Successes", na = "n Missing", phat = "p\u0302",
                                  se = "Standard Error", p = "p-value (2 tail)") %>%
-    flextable::vline(j = 5)
-
+    flextable::vline(j = 5) %>% 
+    flextable::autofit() %>% 
+    flextable::fit_to_width(max_width = 7.5)
+  
 }
-
-
